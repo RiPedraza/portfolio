baf0818b0fc532b76de9599b1f5a3ddb30430930
Ricardo <ricpedraza001@gmail.com>
2023-03-03 19:27:22 -0300

    Modificaciones generales:1)Re-organización de archivos y carpeta. 2)Creación archivo Json. 3)Se agrego style para la ventana Modal. 4)Se termino de programar la información a mostrar en la ventana modal (queda pendiente añadir icons o simbolos de estrellas en lugar de numeros, que describe el nivel de conocimiento).

---
+++ app/javascript.js
@@ -0,0 +1,111 @@
+// IIFE (inmediately invoked function expression) expresión de función inmediatamente invocada:
+//Esta funcion sirve para obtener la fecha
+(function (){
+    date = new Date();
+    year = date.getFullYear();
+    document.getElementById("current_date").innerHTML = year;
+    document.getElementById("footer_date").innerHTML = year;
+})();
+
+
+/**********************Genrar PDF**************************************/
+/*Creado por Parzibyte (https://parzibyte.me)*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Escuchamos el click del botón
+    const $boton = document.querySelector("#btnCrearPdf");
+    $boton.addEventListener("click", () => {
+        const $elementoParaConvertir = document.body; // <-- Aquí puedes elegir cualquier elemento del DOM
+        html2pdf()
+            .set({
+                margin: 0.2,
+                filename: 'Portfolio_Pedraza_Ricardo.pdf',
+                image: {
+                    type: 'jpeg',
+                    quality: 0.98
+                },
+                html2canvas: {
+                    scale: 3, // A mayor escala, mejores gráficos, pero más peso
+                    letterRendering: true,
+                },
+                jsPDF: {
+                    unit: "in",
+                    format: "a4",
+                    orientation: 'portrait' // landscape o portrait
+                }
+            })
+            .from($elementoParaConvertir)
+            .save()
+            .catch(err => console.log(err));
+    });
+});
+
+
+/****** Ventana Modal: Section Skills******/
+const ventanaModal = document.querySelector("#ventana-modal");
+const cerrarModal = document.querySelector("#cerrar-modal");
+
+cerrarModal.addEventListener("click",()=>{
+    ventanaModal.close();
+});
+
+const clickCard = document.querySelector(".main__section__Skills__box");
+clickCard.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const targetClick = e.target;
+    var targetElement = "";
+    var targetElementName = "";
+    
+    /*if(targetClick.classList[0] != 'main__section__Skills__box'){
+        meter todo adentro..
+    }*/
+
+    if(targetClick.classList[0] == 'card-skills'){
+        targetElement = targetClick.classList[1];
+    }else{
+        targetElement = targetClick.parentNode.classList[1];
+    }
+    const cardSkills = document.getElementsByClassName(targetElement)[0];
+    
+    //obtenemos el SRC de la imagen como ruta relativa:
+    const src = cardSkills.children[0].src; //recuperamos el src del elemento (target)
+    const inicio = src.indexOf('/assets'); //indice inicio del corte
+    const fin = src.length; //indice fin del corte
+    let cadenaCortada = src.substring(inicio,fin); //cortamos la ruta deseada
+    cadenaCortada = "." + cadenaCortada;  // agregamos el punto
+    
+    
+    ventanaModal.showModal();   
+    ventanaModal.children[0].innerHTML = cardSkills.children[1].innerHTML;
+    ventanaModal.children[1].src = cadenaCortada;
+    
+    //Consumiendo datos Json. Guía: https://developer.mozilla.org/es/docs/Learn/JavaScript/Objects/JSON
+    const requestURL = 'app/skillsDB.json';
+    const request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        const dbJson = request.response;
+        skills(dbJson);
+    };
+
+    function skills(dbJson){
+        const nameSkill = targetElement;
+        ventanaModal.children[2].innerHTML = dbJson[nameSkill][0]['nivel']+"<hr>";
+        ventanaModal.children[3].textContent = dbJson[nameSkill][0]['detalle'];
+        ventanaModal.children[4].textContent = dbJson[nameSkill][0]['seguimiento'];
+
+    };
+    
+
+
+
+    
+    
+    ventanaModal.style.setProperty("max-width", "60ch");
+
+});
+
+
+
