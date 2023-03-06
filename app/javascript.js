fd6df448c25c823617c30b9813050af24d65b37d
Ricardo <ricpedraza001@gmail.com>
2023-03-06 14:16:40 -0300

    Se agrego la funcionalidad de volver a la barra de inicio en cada sección de la página, y se reubica el ID=page2el para el Pagebreak (se volvió a establecer el tamaño de hoja A4)

--- app/javascript.js
+++ app/javascript.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
         html2pdf()
             .set({
                 margin: 0.3,
-                filename: 'Portfolio_Pedraza_Ricardo_A3.pdf',
+                filename: 'Portfolio_Pedraza_Ricardo_A4.pdf',
                 image: {
                     type: 'jpeg',
                     quality: 0.98
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 },
                 jsPDF: {
                     unit: "in",
-                    format: "a3",
+                    format: "a4",
                     orientation: 'portrait' // landscape o portrait
                 },
                 pagebreak: { 
@@ -60,55 +60,48 @@ clickCard.addEventListener("click",(e)=>{
     var targetElement = "";
     var targetElementName = "";
     
-    /*if(targetClick.classList[0] != 'main__section__Skills__box'){
-        meter todo adentro..
-    }*/
+    if(targetClick.classList[0] != 'main__section__Skills__box'){
+    
+        if(targetClick.classList[0] == 'card-skills'){
+            targetElement = targetClick.classList[1];
+        }else{
+            targetElement = targetClick.parentNode.classList[1];
+        }
+        const cardSkills = document.getElementsByClassName(targetElement)[0];
+        
+        //obtenemos el SRC de la imagen como ruta relativa:
+        const src = cardSkills.children[0].src; //recuperamos el src del elemento (target)
+        const inicio = src.indexOf('/assets'); //indice inicio del corte
+        const fin = src.length; //indice fin del corte
+        let cadenaCortada = src.substring(inicio,fin); //cortamos la ruta deseada
+        cadenaCortada = "." + cadenaCortada;  // agregamos el punto
+        
+        
+        ventanaModal.showModal();   
+        ventanaModal.children[0].innerHTML = cardSkills.children[1].innerHTML;
+        ventanaModal.children[1].src = cadenaCortada;
+        
+        //Consumiendo datos Json. Guía: https://developer.mozilla.org/es/docs/Learn/JavaScript/Objects/JSON
+        const requestURL = 'app/skillsDB.json';
+        const request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+        request.onload = function() {
+            const dbJson = request.response;
+            skills(dbJson);
+        };
 
-    if(targetClick.classList[0] == 'card-skills'){
-        targetElement = targetClick.classList[1];
-    }else{
-        targetElement = targetClick.parentNode.classList[1];
+        function skills(dbJson){
+            const nameSkill = targetElement;
+            ventanaModal.children[2].innerHTML = dbJson[nameSkill][0]['nivel']+"<hr>";
+            ventanaModal.children[3].textContent = dbJson[nameSkill][0]['detalle'];
+            ventanaModal.children[4].textContent = dbJson[nameSkill][0]['seguimiento'];
+
+        };
+
+        ventanaModal.style.setProperty("max-width", "60ch");
     }
-    const cardSkills = document.getElementsByClassName(targetElement)[0];
-    
-    //obtenemos el SRC de la imagen como ruta relativa:
-    const src = cardSkills.children[0].src; //recuperamos el src del elemento (target)
-    const inicio = src.indexOf('/assets'); //indice inicio del corte
-    const fin = src.length; //indice fin del corte
-    let cadenaCortada = src.substring(inicio,fin); //cortamos la ruta deseada
-    cadenaCortada = "." + cadenaCortada;  // agregamos el punto
-    
-    
-    ventanaModal.showModal();   
-    ventanaModal.children[0].innerHTML = cardSkills.children[1].innerHTML;
-    ventanaModal.children[1].src = cadenaCortada;
-    
-    //Consumiendo datos Json. Guía: https://developer.mozilla.org/es/docs/Learn/JavaScript/Objects/JSON
-    const requestURL = 'app/skillsDB.json';
-    const request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-        const dbJson = request.response;
-        skills(dbJson);
-    };
-
-    function skills(dbJson){
-        const nameSkill = targetElement;
-        ventanaModal.children[2].innerHTML = dbJson[nameSkill][0]['nivel']+"<hr>";
-        ventanaModal.children[3].textContent = dbJson[nameSkill][0]['detalle'];
-        ventanaModal.children[4].textContent = dbJson[nameSkill][0]['seguimiento'];
-
-    };
-    
-
-
-
-    
-    
-    ventanaModal.style.setProperty("max-width", "60ch");
-
 });
 
 
